4685afe2073235db49ef0bdb242903da8a7df7b8
ania.liulka <anialiulka@gmail.com>
2023-08-04 09:52:12 -0400

    Edited the logic of the code

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,23 +1,17 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { List, Button } from './FeedbackOptions.styled';
 
-export class FeedbackOptions extends Component {
-  render() {
-    const { options, onLeaveFeedback } = this.props;
-    const keys = Object.keys(options);
-
-    return (
-      <List>
-        {keys.map((key, index) => (
-          <li key={index}>
-            <Button onClick={() => onLeaveFeedback(key)}>{key}</Button>
-          </li>
-        ))}
-      </List>
-    );
-  }
-}
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <List>
+      {options.map((key, index) => (
+        <li key={index}>
+          <Button onClick={() => onLeaveFeedback(key)}>{key}</Button>
+        </li>
+      ))}
+    </List>
+  );
+};
 
 FeedbackOptions.propTypes = {
   options: PropTypes.object.isRequired,
